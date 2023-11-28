71347eaaaad5b8af5f6fa043205c27ef0496b9d3
kratos155 <mehdi.zinelabidine@esprit.tn>
2023-11-28 10:28:32 +0100

    Adding chambre list and chambre update

---
+++ src/app/pages/chambre-list/chambre-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chambre-list',
+  templateUrl: './chambre-list.component.html',
+  styleUrls: ['./chambre-list.component.css']
+})
+export class ChambreListComponent {
+
+}
---
+++ src/app/pages/chambre-update/chambre-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chambre-update',
+  templateUrl: './chambre-update.component.html',
+  styleUrls: ['./chambre-update.component.css']
+})
+export class ChambreUpdateComponent {
+
+}
